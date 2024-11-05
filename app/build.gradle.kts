117442c746a149a7a1b1e875a8d54169f582989d
monotoshghosh <monotoshghosh49@gmail.com>
2024-11-05 22:05:59 +0530

    Version 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.monotoshghosh.tictactoe"
         minSdk = 22
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
