1b17dd5bf03187b199e472dd7077c36f04fc583a
monotoshghosh <monotoshghosh49@gmail.com>
2025-07-31 21:58:10 +0530

    EdgeToEdge

--- app/src/main/java/com/monotoshghosh/tictactoe/team_selection.kt
+++ app/src/main/java/com/monotoshghosh/tictactoe/team_selection.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,6 +14,7 @@ import com.monotoshghosh.tictactoe.databinding.ActivityTeamSelectionBinding
 class team_selection : AppCompatActivity() {
     private lateinit var binding: ActivityTeamSelectionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityTeamSelectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
