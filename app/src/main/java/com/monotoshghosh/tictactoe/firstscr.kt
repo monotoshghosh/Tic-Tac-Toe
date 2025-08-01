dba5b28dee77465adcd4c7ef38d02814cd5cfcff
monotoshghosh <monotoshghosh49@gmail.com>
2025-08-01 10:23:20 +0530

    Same For firstScr

--- app/src/main/java/com/monotoshghosh/tictactoe/firstscr.kt
+++ app/src/main/java/com/monotoshghosh/tictactoe/firstscr.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -16,16 +18,22 @@ class firstscr : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()                                       // API 35
         super.onCreate(savedInstanceState)
         binding = ActivityFirstscrBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        hideNavigationBar()
+        // Apply display cutout handling
+        applyDisplayCutout()                                     // API 35
 
-        window.apply {    // SETTING THE STATUS BAR
-            statusBarColor=ContextCompat.getColor(this@firstscr, R.color.white) // BACKGROUND -> WHITE
-            decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR        // ICONS -> DARK
-        }
+        // HIDE NAVIGATION BAR AND STATUS BAR
+        hideSystemBar()
+
+        // Deprecated Code
+//        window.apply {    // SETTING THE STATUS BAR
+//            statusBarColor=ContextCompat.getColor(this@firstscr, R.color.white) // BACKGROUND -> WHITE
+//            decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR        // ICONS -> DARK
+//        }
 
         // FOR BACKGROUND MUSIC -- > START
         // CALLING THE FUNCTION
@@ -42,12 +50,29 @@ class firstscr : AppCompatActivity() {
     private fun startMusic() {
         startService(Intent(this, MusicService::class.java))
     }
+    private fun applyDisplayCutout(){
+        ViewCompat.setOnApplyWindowInsetsListener(binding.firstScrConstLayout) { view, insets ->
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or
+                        WindowInsetsCompat.Type.displayCutout()
+            )
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
 
-    private fun hideNavigationBar() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        val controller = WindowInsetsControllerCompat(window, window.decorView)
-        controller.hide(WindowInsetsCompat.Type.navigationBars())
-        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
+
+    private fun hideSystemBar() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(
+            WindowInsetsCompat.Type.statusBars() or
+                    WindowInsetsCompat.Type.navigationBars()
+        )
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+
 
 }
